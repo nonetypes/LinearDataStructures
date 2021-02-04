07d8433c10498f890e583f4839a9baeb1b0187b9
nonetypes <nonetypes@gmail.com>
2021-02-03 16:53:00 -0800

    More comments, all pop methods return what was popped, __len__ method added

--- linkedlist.py
+++ linkedlist.py
@@ -1,5 +1,5 @@
 # linkedlist.py by nonetypes
-# Last revised on 01/21/2021
+# Last revised on 01/29/2021
 
 class Node:
     """Node obect to make up the links within a LinkedList.
@@ -25,7 +25,9 @@ class LinkedList:
     Multiple argumets can be given to create multiple nodes in the list.
     """
     def __init__(self, *items):
+        # Only create nodes if the items are not already nodes.
         items = [item if isinstance(item, Node) else Node(item) for item in items]
+        # Assign each node's next_node attribute.
         for i in range(len(items)-1):
             items[i].next_node = items[i+1]
         self.head = items[0] if items else None
@@ -33,16 +35,22 @@ class LinkedList:
     def __repr__(self):
         return str(self.py_list())
 
+    def __len__(self):
+        return len(self.py_list())
+
     def __getitem__(self, index):
         """
         Return an item from an index:
 
             linked = LinkedList('a', 'b', 'c')
             linked[1]                # returns 'b'
-        Negative indexes and slices not supported.
+        Slices not supported.
         """
         if not isinstance(index, int):
             raise TypeError('list indices must be integers')
+        # Negative index support.
+        if index < 0:
+            index = len(self) + index
         i = 0
         link = self.head
         while link is not None:
@@ -50,7 +58,7 @@ class LinkedList:
                 return link.item
             link = link.next_node
             i += 1
-        if index >= i:
+        if index >= i or index < 0:
             raise IndexError('list index out of range')
 
     def __setitem__(self, index, new_item):
@@ -62,6 +70,9 @@ class LinkedList:
         """
         if not isinstance(index, int):
             raise TypeError('list indices must be integers')
+        # Negative index support.
+        if index < 0:
+            index = len(self) + index
         i = 0
         link = self.head
         while link is not None:
@@ -69,13 +80,14 @@ class LinkedList:
                 link.item = new_item
             link = link.next_node
             i += 1
-        if index >= i:
+        if index >= i or index < 0:
             raise IndexError('list index out of range')
 
     def append_left(self, item):
         """Append an item to the beginning of the list.
         """
         item = item if isinstance(item, Node) else Node(item)
+        # Create a new head. The old head becomes the new head's next_node.
         item.next_node = self.head
         self.head = item
 
@@ -86,6 +98,9 @@ class LinkedList:
         if self.head is None:
             self.head = item
         else:
+            # Start with the head.
+            # Work through each node until the next_node is None (the tail),
+            # and assign the given item (new node) to its next_node.
             link = self.head
             while link.next_node is not None:
                 link = link.next_node
@@ -94,6 +109,7 @@ class LinkedList:
     def append(self, item):
         """Append an item to the end of the list.
         """
+        # For ease of use. Functions like built-in [].append()
         self.append_right(item)
 
     def insert(self, index, item):
@@ -102,73 +118,91 @@ class LinkedList:
         if not isinstance(index, int):
             raise TypeError('list indices must be integers')
         item = item if isinstance(item, Node) else Node(item)
+        # Negative index support.
+        if index < 0:
+            index = len(self) + index
         if index == 0:
-            item.next_node = self.head
-            self.head = item
+            self.append_left(item)
         else:
             inserted = False
-            i = 0
+            # Start at 1 rather than 0 for i because the link preceding the
+            # given index is what must be altered.
+            i = 1
             link = self.head
             while link is not None:
-                if i+1 == index:
+                if i == index:
                     item.next_node = link.next_node
                     link.next_node = item
                     inserted = True
                 link = link.next_node
                 i += 1
-            if index >= i and not inserted:
+            # Assume index out of range if nothing was inserted.
+            if not inserted:
                 raise IndexError('list index out of range')
 
     def pop_left(self):
-        """Remove the left most item in the list.
+        """Remove the left most item in the list and return it.
         """
         if self.head is None:
             raise IndexError('pop from empty list')
         else:
+            # Reassign the head to the head's next_node effectively deleting it
+            popped_item = self.head.item
             self.head = self.head.next_node
+            return popped_item
 
     def pop_right(self):
-        """Remove the right most item in the list.
+        """Remove the right most item in the list and return it.
         """
         if self.head is None:
             raise IndexError('pop from empty list')
         if self.head.next_node is None:
+            popped_item = self.head.item
             self.head = None
         else:
             link = self.head
             while link.next_node is not None:
+                # Detect if the node after the next is the tail because the
+                # preceding node is what must be altered in order to delete it.
                 if link.next_node.next_node is None:
+                    popped_item = link.next_node.item
                     link.next_node = None
                 else:
                     link = link.next_node
+        return popped_item
 
     def pop(self, index=None):
-        """Remove an item at the given index from the list.
+        """Remove an item at the given index from the list and return it.
 
         Remove the last item if index is omitted.
         """
         if index is None:
-            self.pop_right()
+            return self.pop_right()
         else:
             if not isinstance(index, int):
                 raise TypeError('list indices must be integers')
             elif self.head is None:
                 raise IndexError('pop from empty list')
-            elif index == 0:
-                self.pop_left()
+            # Negative index support.
+            if index < 0:
+                index = len(self) + index
+            if index == 0:
+                return self.pop_left()
             else:
-                popped = False
-                i = 0
+                # Start at 1 rather than 0 for i because the link preceding the
+                # given index is what must be altered.
+                i = 1
                 link = self.head
                 while link is not None:
-                    if i+1 == index:
+                    if i == index:
                         if link.next_node is not None:
+                            popped_item = link.next_node.item
                             link.next_node = link.next_node.next_node
-                            popped = True
+                            return popped_item
                     link = link.next_node
                     i += 1
-                if index >= i and not popped:
-                    raise IndexError('list index out of range')
+                # Assume index out of range if function got this far.
+                raise IndexError('list index out of range')
 
     def contains(self, item):
         """Return True if the given item is within the list and False otherwise.
@@ -199,7 +233,7 @@ if __name__ == "__main__":
     linked[2] = 'two'
     print(linked.contains(2))
     print(linked)
-    linked.pop(2)
+    print(f'"{linked.pop(-3)}" was popped.')
     print(linked)
     linked.pop_left()
     linked.pop_right()
--- queue.py
+++ queue.py
@@ -1,5 +1,5 @@
 # queue.py by nonetypes
-# Last revised on 01/16/2021
+# Last revised on 01/29/2021
 
 class Queue:
     """List like object. First in, first out.
@@ -12,6 +12,11 @@ class Queue:
         """Return a printable string version of self.items
         """
         return str(self.items)
+
+    def __len__(self):
+        """Return the len() of self.items
+        """
+        return len(self.items)
 
     def __getitem__(self, index):
         """
@@ -29,7 +34,7 @@ class Queue:
 
     def __add__(self, other_item):
         """
-        Concatenation support:
+        Concatenation support. Returns a concatenated list and not a Queue.
 
             queue = Queue(1, 2, 3)
             queue + 4               # returns [1, 2, 3, 4]
@@ -51,14 +56,17 @@ class Queue:
         self.items += [item]
 
     def pop(self, index=None):
-        """Remove an item at given index from the queue.
+        """Remove an item at given index from the queue and return it.
 
         Remove the first item if index is omitted.
         """
         if index is not None:
+            popped_item = self.items[index]
             del self.items[index]
         else:
+            popped_item = self.items[0]
             del self.items[0]
+        return popped_item
 
     def remove(self, item):
         """Remove the given item from the queue.
